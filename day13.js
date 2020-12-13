9f4b3bcf5c2198e06708fa689e533978f11b2105
Katlyn Edwards <kaitea.edwards@gmail.com>
2020-12-13 00:04:58 -0800

    wtf
    i'm not a math major

---
+++ day13.js
@@ -0,0 +1,142 @@
+let input = `939
+17,x,13,19`;
+
+let inputArr = input.split(`\n`);
+departTime = parseInt(inputArr[0]);
+busRoutes = inputArr[1];
+
+busRoutes = busRoutes.split(',')
+
+function part1() {
+  // console.log(`departTime: ${departTime}`)
+  // console.log(`busRoutesStr: ${busRoutes}`)
+  console.log(`routes Arr: ${busRoutes}`)
+  busRoutes.filter(el => el != 'x')
+
+
+  let nearestArr = [];
+  busRoutes.forEach(route => {
+    route = parseInt(route)
+    // console.log('in loop ' + route)
+    let calc = route
+    while (calc <= departTime) {
+      calc += route;
+      // console.log(`new calc ${calc}`)
+    }
+    // console.log(`route: ${route}; nearest: ${calc}`);
+    nearestArr.push(calc);
+  });
+
+  console.log(`nearestArrs: ${nearestArr}`);
+
+  let min = nearestArr[0];
+  let minIndex = 0;
+  for (let i = 0; i < nearestArr.length; i++) {
+    if (nearestArr[i] < min) {
+      minIndex = i;
+      min = nearestArr[i]
+    }
+  }
+
+  console.log(`soonest: ${min}`);
+  console.log(`soonest index: ${minIndex}`);
+  console.log(`departTime: ${departTime}`)
+  diff = min - departTime
+  console.log(`diff: ${diff}`)
+  console.log(busRoutes[minIndex] * diff);
+}
+
+function part2() {
+  // The solution I was writing will take too long
+  // given the output space.
+
+  // LOL OK, so I used wolfram alpha, and got it, but
+  // like that feels bad.
+
+  // Given the input, this is the equations I
+  // generated, in theory you can solve for T
+  // t = 19a + 0;
+  // t = 41b + 9;
+  // t = 743c + 19;
+  // t = 13d + 32;
+  // t = 17e + 33;
+  // t = 29f + 48;
+  // t = 643g + 50;
+  // t = 37h + 56;
+  // t = 23i + 73;
+
+  // t = 1028248271342350 + 2029817890655789 n
+  // so I did 2029817890655789 - 1028248271342350
+
+  busRoutesNoX = busRoutes.slice();
+  busRoutesNoX = busRoutes.filter(el => el != 'x')
+  busRoutesNoX = busRoutesNoX.map(el => parseInt(el))
+  let arrOfTimes = busRoutesNoX.slice();
+
+  shittyLCM = busRoutesNoX.reduce((prevVal, curr) => {
+    return prevVal * curr;
+  }, 1);
+  // 3162341
+  console.log(`shittyLCM: ${shittyLCM}`)
+
+
+  let rotations = 0;
+  while (rotations < 10) {
+    if (allTimesAreValid(arrOfTimes, busRoutes, busRoutesNoX)) {
+      break;
+    }
+    let min = arrOfTimes[0];
+    let minIndex = 0;
+    for (let i = 1; i < arrOfTimes.length; i++) {
+      console.log("arr@index: " + arrOfTimes[i])
+      console.log("min: " + min)
+      if (parseInt(arrOfTimes[i]) < min) {
+        min = arrOfTimes[i];
+        minIndex = i;
+      }
+    }
+    console.log(`min: ${min}, minIndex: ${minIndex}`)
+    arrOfTimes[minIndex] = shittyLCM + busRoutesNoX[minIndex] ;
+    console.log(`incremented one busRoute: ${arrOfTimes}`);
+    rotations++;
+  }
+
+  let min = arrOfTimes[0];
+  for (let i = 0; i < arrOfTimes; i++) {
+    if (arrOfTimes[i] < min) {
+      min = arrOfTimes[i];
+    }
+  }
+
+  console.log(min);
+}
+
+function allTimesAreValid(arrOfTimes, busRoutes, busRoutesNoX) {
+  let t = arrOfTimes[0];
+  console.log(`t: ${t}`);
+  console.log(`busRoutes: ${busRoutes}`);
+  console.log(`busRoutesNoX: ${busRoutesNoX}`);
+  for ( let i = 1; i < busRoutes.length; i++ ) {
+    if (busRoutes[i] == 'x') {
+       continue;
+    }
+    original = parseInt(busRoutes[i])
+    console.log(`original: ${original}`);
+    console.log(`busRoutesNoX: ${busRoutesNoX}`)
+    arrIndex = busRoutesNoX.indexOf(original)
+    console.log(`arrindex: ${arrIndex}`)
+    console.log(`i: ${i}, arrOfTimes[arrIndex]: ${arrOfTimes[arrIndex]}`)
+    console.log(`t + i: ${t + i}`)
+    if (t + i != arrOfTimes[i]) {
+      console.log('returning false!')
+      return false;
+    }
+  }
+  console.log("*****************")
+  return true;
+
+}
+
+part2();
+
+
